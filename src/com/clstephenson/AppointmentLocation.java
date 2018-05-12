53371723ecc572c68671673d64d5649b158f19db
Chris Stephenson <clstephenson@gmail.com>
2018-05-12 16:45:48 -0700

    Adds timezone string to enum with method to retreive it as a ZoneId

--- src/com/clstephenson/AppointmentLocation.java
+++ src/com/clstephenson/AppointmentLocation.java
@@ -1,19 +1,26 @@
 package com.clstephenson;
 
+import java.time.ZoneId;
+
 public enum AppointmentLocation {
-    PHOENIX ("Phoenix, Arizona"),
-    NEW_YORK ("New York, New York"),
-    LONDON ("London, England"),
-    ONLINE ("Online");
+    PHOENIX ("Phoenix, Arizona", "America/Phoenix"),
+    NEW_YORK ("New York, New York", "America/New_York"),
+    LONDON ("London, England", "Europe/London");
 
     private final String location;
+    private final String timeZone;
 
     @Override
     public String toString() {
         return location;
     }
 
-    private AppointmentLocation(String location) {
+    public ZoneId getTimeZoneId() {
+        return ZoneId.of(timeZone);
+    }
+
+    private AppointmentLocation(String location, String timeZone) {
         this.location = location;
+        this.timeZone = timeZone;
     }
 }
